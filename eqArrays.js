3c9f604077f1bb5f6d20a3f301cf6ba9a2a3a8f5
Vanessa Mugoa <vmugoa@live.ca>
2023-05-25 22:26:47 -0400

    eqArrays function: compares two arrays and returns true or false, based on a perfect match

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+//See results of examples below to test code://
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // false
+console.log(eqArrays(["Sam", "Vanessa", "Jame"], ["Jane", "Sam", "Vanessa"])); // false
