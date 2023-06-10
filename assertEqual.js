2d8a2823b94a58b8de82990fd7bba54484055731
Vanessa M <121637795+vmugoa@users.noreply.github.com>
2023-06-09 21:27:03 -0400

    Update assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,17 +1,16 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
+// ASSERT EQUAL FUNCTION
 
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }   else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`); // the actual and expected are perfectly equal
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`); // the actual and expected are NOT perfectly equal
   }
-  return assertEqual;
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("hello", "HELLO");
-assertEqual("poop", "poop");
-assertEqual(23899, 31);
+// LET'S TEST OUR CODE! 
+assertEqual("Lighthouse Labs", "Bootcamp"); // => 🛑🛑🛑 Assertion Failed: Lighthouse Labs !== Bootcamp
+assertEqual(1, 1); // => ✅✅✅ Assertion Passed: 1 === 1
+assertEqual("hello", "HELLO"); // => 🛑🛑🛑 Assertion Failed: hello !== HELLO
+assertEqual("poop", "poop"); // => ✅✅✅ Assertion Passed: poop === poop
+assertEqual(23899, 31); // => 🛑🛑🛑 Assertion Failed: 23899 !== 31
