94e89daac706f7fc9bb989e15b8c2f09a5e3205f
Vanessa Mugoa <vmugoa@live.ca>
2023-06-09 22:12:16 -0400

    Compares two arrays and returns true/false if perfectly equal

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,5 @@
+// EQUALS ARRAY FUNCTION
+
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -11,10 +13,10 @@ const eqArrays = function(array1, array2) {
   }
 };
 
-//See results of examples below to test code://
+// LET'S TEST OUR CODE!
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // false
-console.log(eqArrays(["Sam", "Vanessa", "Jame"], ["Jane", "Sam", "Vanessa"])); // false
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+console.log(eqArrays(["Sam", "Vanessa", "Jame"], ["Jane", "Sam", "Vanessa"])); // => false
