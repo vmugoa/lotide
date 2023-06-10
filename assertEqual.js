0c55262e2f727a56cfe19e7b351135de368e5d68
Vanessa Mugoa <vmugoa@live.ca>
2023-06-09 22:09:46 -0400

    Asserts if two parameters are equal

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 // ASSERT EQUAL FUNCTION
 
-=======
-// FUNCTION IMPLEMENTATION
->>>>>>> 0ca98c8 (Updated to include notes)
-=======
-// ASSERT EQUAL FUNCTION
-
->>>>>>> 68f204e (Updated)
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`); // the actual and expected are perfectly equal
