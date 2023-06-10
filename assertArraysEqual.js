bdecdf28b0b18019a40c95fe45741e42f8abd35f
Vanessa Mugoa <vmugoa@live.ca>
2023-06-09 22:14:29 -0400

    Asserts if two arrays are perfectly equal

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -27,8 +27,6 @@ console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // ✅✅✅ Assertion Pas
 console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])); // 🛑🛑🛑 Assertion Failed: [1,2,3] !== [3,2,1]
 console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])); // ✅✅✅ Assertion Passed: [1,2,3] === [1,2,3]
 console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3])); // 🛑🛑🛑 Assertion Failed: [1,2,3] !== [1,2,3]
-<<<<<<< HEAD
 console.log(assertArraysEqual(["Sam", "Vanessa", "Jame"], ["Jane", "Sam", "Vanessa"])); // 🛑🛑🛑 Assertion Failed: [Sam,Vanessa,Jame] !== [Jane,Sam,Vanessa]
-=======
 console.log(assertArraysEqual(["Sam", "Vanessa", "Jame"], ["Jane", "Sam", "Vanessa"])); // 🛑🛑🛑 Assertion Failed: [Sam,Vanessa,Jame] !== [Jane,Sam,Vanessa]
->>>>>>> 68f204e (Updated)
+
