e21f235cc9f939e6466d1ee45374ade5fb531e44
Vanessa Mugoa <vmugoa@live.ca>
2023-07-07 23:06:14 -0400

    Refactored this code to adhere to DRY principle - now uses Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,15 +1,18 @@
-// ASSERT EQUAL FUNCTION
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-// HEAD FUNCTION
-const head = require('../head');
+describe("#head", () => {
 
-// LET'S TEST OUR CODE:
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-assertEqual(head([5,6,7]), 5);
-// => 5
-// => ✅✅✅ Assertion Passed: 5 === 5
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
 
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// => Hello
-// => ✅✅✅ Assertion Passed: Hello === Hello
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+  
+});
