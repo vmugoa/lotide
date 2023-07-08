fbb540675b5959e2bc2c87a264ccb52f08eb38c5
Vanessa Mugoa <vmugoa@live.ca>
2023-07-07 22:40:57 -0400

    Refactored this code to adhere to DRY principle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,11 @@
+// ASSERT ARRAYS EQUAL FUNCTION:
+const assertArraysEqual = require('../assertArraysEqual');
+
 // MIDDLE FUNCTION
 const middle = require('../middle');
 
 // LET'S TEST OUR CODE!
-console.log(middle([1, 2, 3])); // Output: [2]
-console.log(middle([1, 2, 3, 4])); // Output: [2, 3]
-console.log(middle([1])); // Output: []
+console.log(middle([1, 2, 3])); // [2]
+console.log(middle([1, 2, 3, 4])); // [2, 3]
+console.log(middle([1])); // []
+console.log(assertArraysEqual(middle([1, 2, 3]), [2])); // ✅✅✅ Assertion Passed: [2] === [2]
