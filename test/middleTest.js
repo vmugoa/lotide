5b52af98b305dde1e702400f32ff7b40dc643b6a
Vanessa Mugoa <vmugoa@live.ca>
2023-07-07 22:39:15 -0400

    Refactored this code to adhere to DRY principle

---
+++ test/middleTest.js
@@ -0,0 +1,7 @@
+// MIDDLE FUNCTION
+const middle = require('../middle');
+
+// LET'S TEST OUR CODE!
+console.log(middle([1, 2, 3])); // Output: [2]
+console.log(middle([1, 2, 3, 4])); // Output: [2, 3]
+console.log(middle([1])); // Output: []
